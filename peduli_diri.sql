d4ed2965759d6c837c45b8ba698673ebbfe66ebf
Ari Setiawan <88638108+EliterDaneo@users.noreply.github.com>
2022-03-10 13:10:18 +0700

    Add files via upload

---
+++ peduli_diri.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 10, 2022 at 07:08 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `peduli_diri`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catatan`
+--
+
+CREATE TABLE `catatan` (
+  `id_catatan` int(11) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `waktu` time NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
+  `suhu` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `catatan`
+--
+
+INSERT INTO `catatan` (`id_catatan`, `nik`, `tanggal`, `waktu`, `lokasi`, `suhu`) VALUES
+(5, '1234567', '2022-03-10', '09:57:00', 'wonosobo', '37'),
+(6, '1234567', '2022-03-10', '00:13:00', 'wonosobo city', '35'),
+(7, '1234567', '2022-03-07', '10:19:00', 'wonosobo', '36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `nama_lengkap` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nik`, `nama_lengkap`) VALUES
+(2, '1234567', 'ari');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `catatan`
+--
+ALTER TABLE `catatan`
+  ADD PRIMARY KEY (`id_catatan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `catatan`
+--
+ALTER TABLE `catatan`
+  MODIFY `id_catatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
